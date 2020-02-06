cf429c645fa557859f5b7e484baaf1b2a2bf4e0c
agutierrez72 <agutierrez72@ucmerced.edu>
2020-02-06 09:39:25 -0800

    create endpoint to checkWinner

--- src/app.cpp
+++ src/app.cpp
@@ -1,5 +1,5 @@
 #include <server.h>
-
+#include <lib.h>
 using namespace ucm;
 
 int main(int argc, char** argv){
@@ -8,5 +8,26 @@ int main(int argc, char** argv){
 
     server.renderHTML("/", "index.html");
 
+    
+
+    server.route("/checkWinner", [&](const request& req, response& res){
+        if(req.has_params({"team"})){
+            if(req.has_params({"team"})){
+            std::string team = req.url_params.get("team");
+
+            bool correct = checkWinner(team);
+
+            if(correct)
+                res.sendHTML("Correct");
+            else
+                res.sendHTML("Incorrect");
+            }
+            else
+                res.sendError400();
+        }
+    });
+    
     server.run();
+
+
 }
